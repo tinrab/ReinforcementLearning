bb20a97b5c658920ff0606178336898862042eda
tinrab <tin.rabzelj@gmail.com>
2017-02-14 14:35:34 +0100

    q learning test

--- Tests/QLearningTest.cs
+++ Tests/QLearningTest.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ReinforcementLearning;
 
 namespace Tests
 {
@@ -6,8 +8,74 @@ namespace Tests
   public class QLearningTest
   {
     [TestMethod]
-    public void SimpleTest()
+    public void SimpleGridPathTest()
     {
+      var random = new Random(1337);
+      var gridSize = 16;
+      var grid = new int[gridSize, gridSize];
+      var ql = new QLearning(gridSize * gridSize, 4, new EpsilonGreedyExplorationPolicy(0.0), 0.3, 0.8);
+      var pathLength = 20;
+      var maxReward = 0.0;
+
+      for (var x = 0; x < gridSize; x++) {
+        for (var y = 0; y < gridSize; y++) {
+          grid[x, y] = random.Next(0, 100);
+        }
+      }
+
+      // train
+      for (var i = 0; i < 1000; i++) {
+        var x = random.Next(0, gridSize);
+        var y = random.Next(0, gridSize);
+        var currentReward = 0.0;
+
+        for (var j = 0; j < pathLength; j++) {
+          var previousState = x + y * gridSize;
+          var a = (Action) ql.SelectAction(previousState);
+
+          switch (a) {
+            case Action.UP:
+              y++;
+              break;
+            case Action.DOWN:
+              y--;
+              break;
+            case Action.LEFT:
+              x--;
+              break;
+            case Action.RIGHT:
+              x++;
+              break;
+          }
+
+          var r = 0;
+          if (x < 0 || x >= gridSize || y < 0 || y >= gridSize) {
+            x = x < 0 ? 0 : x >= gridSize ? gridSize - 1 : x;
+            y = y < 0 ? 0 : y >= gridSize ? gridSize - 1 : y;
+          } else {
+            r = grid[x, y];
+          }
+
+          currentReward += r;
+
+          var nextState = x + y * gridSize;
+          ql.Learn(previousState, (int) a, r, nextState);
+        }
+
+        if (currentReward > maxReward) {
+          maxReward = currentReward;
+        }
+      }
+
+      Assert.AreEqual(896, maxReward);
+    }
+
+    private enum Action
+    {
+      UP = 0,
+      DOWN = 1,
+      LEFT = 2,
+      RIGHT = 3
     }
   }
 }
